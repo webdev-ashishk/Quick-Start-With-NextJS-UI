5c85919748407b08db26c215e95b65df9e5a7de5
webdev-ashishk <webdev.ashishk@gmail.com>
2023-09-21 16:00:45 +0530

    nav bar is ready but not responsive

---
+++ app/blogs/page.jsx
@@ -0,0 +1,9 @@
+export default function Blogs() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold text-purple-400 h-screen text-center pt-10">
+        List of Blogs cames Here!!!
+      </h1>
+    </main>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
+import NavBar from "@/components/NavBar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,7 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          <NavBar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
